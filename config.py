79b79b59e0c77d081b8d5f1cb0eb6810a0d1795a
Stardust <arkasarkar235@gmail.com>
2025-06-20 00:34:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
+API_ID = "10130264" #API ID get it from telegram.org
+API_HASH = "aa0734d88e68fa88b19504fc1124af9b" #API HASH get it from telegram.org
+BOT_TOKEN = "8132519140:AAFWVNE25m6Bb33IGtVjXwMUQcEisTbviUA" #Bot Token get it from @BotFather
 URL_A = "" #News RSS Feed Url 2
-#URL_B = "" #News RSS Feed Url 2
+URL_B = "" #News RSS Feed Url 2
 START_PIC = "" #Startup Pic
 MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
 ADMINS = "" #Admin ID
