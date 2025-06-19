964a880a4f39a5191d51c317a30537a268ed406b
Stardust <arkasarkar235@gmail.com>
2025-06-20 00:45:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 API_ID = "10130264" #API ID get it from telegram.org
 API_HASH = "aa0734d88e68fa88b19504fc1124af9b" #API HASH get it from telegram.org
 BOT_TOKEN = "8132519140:AAFWVNE25m6Bb33IGtVjXwMUQcEisTbviUA" #Bot Token get it from @BotFather
-URL_A = "" #News RSS Feed Url 2
-URL_B = "" #News RSS Feed Url 2
-START_PIC = "" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
-ADMINS = "" #Admin ID
+URL_A = "https://www.animenewsnetwork.com/all/rss.xml" #News RSS Feed Url 2
+URL_B = "https://www.imdb.com/news/movie/rss" #News RSS Feed Url 2
+START_PIC = "https://i.ibb.co/5gCMx8rj/x.jpg" #Startup Pic
+MONGO_URI = "mongodb+srv://sarkarchameli73:UeKwUuIBxmQktF90@cluster0.0attx.mongodb.net/" #Databse uri get it from https://www.mongodb.com/
+ADMINS = "7089676783" #Admin ID
